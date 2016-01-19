9023027baef0c0a76e2b3f28505ee75bd2cff380
Selle <michaeldejong26@gmail.com>
2016-01-19 13:48:55 +0100

    Commit

--- Project 2/Entities.py
+++ Project 2/Entities.py
@@ -1,29 +1,31 @@
 import random
 
 class Vector2D():
-    def __init__(self, positionX, positionY):
-        self.X = positionX
-        self.Y = positionY
+    def __init__(self, x, y):
+        self.X = x
+        self.Y = y
+    def __str__(self):
+        return "(" + str(self.X) + ", " + str(self.Y) + ")"
 
 class Dice():
-    def __init__(self, Position, Size):
-        self.Number = 1
-        self.Position = Position
-        self.Size = Size
+    def __init__(self, position, size):
+        self.number = 1
+        self.position = position
+        self.size = size
     def roll(self): 
-        self.Number = random.randint(1,6)
+        self.number = random.randint(1,6)
     def getTexture(self):
         return {
-            1: "Assets/dice/dice_1.png", 
-            2: "Assets/dice/dice_2.png",
-            3: "Assets/dice/dice_3.png",
-            4: "Assets/dice/dice_4.png",
-            5: "Assets/dice/dice_5.png",
-            6: "Assets/dice/dice_6.png"
-        }.get(self.Number, 1)
+            1: "Content/dice/dice_1.png", 
+            2: "Content/dice/dice_2.png",
+            3: "Content/dice/dice_3.png",
+            4: "Content/dice/dice_4.png",
+            5: "Content/dice/dice_5.png",
+            6: "Content/dice/dice_6.png"
+        }.get(self.number, 1)
     def render(self, screen):
         screen.blit(
             pygame.transform.scale(pygame.image.load(self.getTexture).convert_alpha()), 
-            (self.Size.X, self.Size.Y), 
-            (self.Position.X, self.Position.Y)
+            (self.size.X, self.size.Y), 
+            (self.position.X, self.position.Y)
         )
