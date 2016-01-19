04ff1b99bb20c3c59dfbeb87804fe1c7f177d664
Selle <michaeldejong26@gmail.com>
2016-01-19 13:25:51 +0100

    Dice + Vector2d

---
+++ DEV 2 Game template/Dice.py
@@ -0,0 +1,26 @@
+﻿import random
+
+class Dice():
+    def __init__(self, Position, Size):
+        self.Number = 1
+        self.Position = Position
+        self.Size = Size
+    def roll(self): 
+        self.Number = random.randint(1,6)
+    def getTexture(self):
+        return {
+            1: "Assets/dice/dice_1.png", 
+            2: "Assets/dice/dice_2.png",
+            3: "Assets/dice/dice_3.png",
+            4: "Assets/dice/dice_4.png",
+            5: "Assets/dice/dice_5.png",
+            6: "Assets/dice/dice_6.png"
+        }.get(self.Number, 1)
+    def render(self, screen):
+        screen.blit(
+            pygame.transform.scale(pygame.image.load(self.getTexture).convert_alpha()), 
+            (self.Size.X, self.Size.Y), 
+            (self.Position.X, self.Position.Y)
+        )
+
+
---
+++ DEV 2 Game template/Vector2D.py
@@ -0,0 +1,6 @@
+﻿class Vector2D():
+    def __init__(self, positionX, positionY):
+        self.X = positionX
+        self.Y = positionY
+
+
