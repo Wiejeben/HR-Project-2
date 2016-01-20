dbcd2edb62d9634a88e37a379e3a6e0d528b2b5e
Rick Zegelaar <rickzegelaar@gmail.com>
2016-01-20 10:53:33 +0100

    Merged the menu into the correct class, integrated with app structure from master

--- Project 2/Main.py
+++ Project 2/Main.py
@@ -27,7 +27,7 @@ pygame.display.set_caption("Groep 3: Buy a Ride")
 def Main():
     # Create initial game instance
     app = AppState()
-    menu = Menu(background)
+    menu = Menu(background, app)
     game = Game(background, 4)
     speed = 0.2
 
@@ -36,19 +36,15 @@ def Main():
 
         # Events
         for event in pygame.event.get():
-            if event.type == QUIT:
-                app.exit()
-
-            if event.type == MOUSEBUTTONDOWN:
-                app.start()
-
-            elif event.type == KEYDOWN:
+            if app.state == "Menu":
+                menu.handleInputs(event)
+            if event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     app.togglePause()
 
         if app.state == "Menu":
             # Render menu
-            menu.load()
+            menu.draw()
 
         elif app.state == "Game":
             # Show game
@@ -58,7 +54,8 @@ def Main():
                 game.load()
 
         # Render
-        screen.blit(background, (0, 0))
+        if(app.state == "Game"):
+            screen.blit(background, (0, 0))
         pygame.display.flip()
 
         # Run at 5 FPS
--- Project 2/Menu.py
+++ Project 2/Menu.py
@@ -1,16 +1,41 @@
 ﻿import pygame
+from UIToolKit.Button import *
+from UIToolKit.ImageUtils import *
 
 class Menu(object):
-    def __init__(self, background):
+
+    
+    def __init__(self, background, appstate):        
         self.background = background
+        self.ButtonList = []  
+        #Can't we do this better?
+        self.App = appstate
+        self.load()
 
     def load(self):
-        green = 50, 255, 100
-        self.background.fill(green)
-        font = pygame.font.Font(None, 36)
-        text = font.render("Menu", 1, (10, 10, 10))
-        textpos = text.get_rect()
-        textpos.centerx = self.background.get_rect().centerx
-        self.background.blit(text, textpos)
+        startButton = Button(ImageUtils.Screen.get_width() / 2 - 100, 50, 200, 100, "normal_example.png", "hover_example.png", "pressed_example.png", self.startGameFunc)
+        self.ButtonList.append(startButton)
+        settingsButton = Button(ImageUtils.Screen.get_width() / 2 - 100, startButton.PositionY + 200, 200, 100, "normal_example.png", "hover_example.png", "pressed_example.png", self.settingsFunc)
+        self.ButtonList.append(settingsButton)
+        quitButton = Button(ImageUtils.Screen.get_width() / 2 - 100, settingsButton.PositionY + 200, 200, 100, "normal_example.png", "hover_example.png", "pressed_example.png", self.quitFunc)
+        self.ButtonList.append(quitButton)
+
+    def startGameFunc(self):
+        self.App.start()
+
+    def settingsFunc(self):
+        print("Open Settings menu here :D")
+
+    def quitFunc(self):
+        self.App.exit()
+       
+
+    def draw(self):
+        for button in self.ButtonList:
+            button.draw()
+
+    def handleInputs(self, event):
+        for button in self.ButtonList:
+            button.handleInputEvents(event)
 
 
