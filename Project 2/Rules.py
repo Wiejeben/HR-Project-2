6528346a8e9f5b990379de6e6899a9a902b9ecff
Dilan Leeuwendaal <dilan95@hotmail.nl>
2016-01-24 11:06:27 +0100

    Rules page

---
+++ Project 2/Rules.py
@@ -0,0 +1,15 @@
+class Text:
+    def __init__(self, content, position, fontsize, color):
+        self.content = content
+        self.position = position
+        self.fontsize = fontsize
+        self.color = color
+
+    def DrawText(self):
+        return content + position + fontsize + color
+
+print ("Before the game starts")
+print ("At the start, every player gets $10.000")
+print ("Every player gets an “own park”. This is the little board. Here you put every") 
+print ("Every player gets an essential card. You need to complete the objective on the ")
+print ("card in order to win the game.")
