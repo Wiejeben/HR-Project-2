e0c14f8d92ea0b8220583867792f5fe4ecc54f7d
Dilan Leeuwendaal <dilan95@hotmail.nl>
2016-01-25 15:18:47 +0100

    Rules

--- Project 2/Rules.py
+++ Project 2/Rules.py
@@ -1,3 +1,5 @@
+import pygame
+
 class Text:
     def __init__(self, content, position, fontsize, color):
         self.content = content
@@ -10,6 +12,6 @@ class Text:
 
 print ("Before the game starts")
 print ("At the start, every player gets $10.000")
-print ("Every player gets an “own park”. This is the little board. Here you put every") 
+print ("Every player gets an ï¿½own parkï¿½. This is the little board. Here you put every") 
 print ("Every player gets an essential card. You need to complete the objective on the ")
 print ("card in order to win the game.")
